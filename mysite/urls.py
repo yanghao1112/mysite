ae103ef58d4ada026f195d2a7996bac2ad8f6c47
yanghao <494608821@qq.com>
2016-10-16 14:14:03 +0800

    Initial commit

---
+++ mysite/urls.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+from django.conf.urls import url
+from django.contrib import admin
+from blog import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^index/$', views.index)
+]
